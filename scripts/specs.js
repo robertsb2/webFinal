3d53daa52b2c94f553e1070b80aa3e676b9152c2
Unknown <Shamacron@gmail.com>
2018-03-09 11:18:35 -0700

    Table Functionality
    Finished the functionality required for the table in the product specs page.

--- scripts/specs.js
+++ scripts/specs.js
@@ -1,78 +1,121 @@
 $(document).ready(function () {
-    $('#item0').on('mouseover', function () {
-        $(this).css({
+    $('#set0').on('mouseover', function () {
+        $('#item0').css({
+            "background-color": "#ff1"
+        });
+        $('#item1').css({
             "background-color": "#ff1"
         });
     });
-    $('#item0').on('mouseout', function () {
-        $(this).css({
+    $('#set0').on('mouseout', function () {
+        $('#item0').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item1').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item1').on('mouseover', function () {
-        $(this).css({
+    $('#set1').on('mouseover', function () {
+        $('#item2').css({
+            "background-color": "#ff1"
+        });
+        $('#item3').css({
             "background-color": "#ff1"
         });
     });
-    $('#item1').on('mouseout', function () {
-        $(this).css({
+    $('#set1').on('mouseout', function () {
+        $('#item2').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item3').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item2').on('mouseover', function () {
-        $(this).css({
+    $('#set2').on('mouseover', function () {
+        $('#item4').css({
+            "background-color": "#ff1"
+        });
+        $('#item5').css({
             "background-color": "#ff1"
         });
     });
-    $('#item2').on('mouseout', function () {
-        $(this).css({
+    $('#set2').on('mouseout', function () {
+        $('#item4').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item5').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item3').on('mouseover', function () {
-        $(this).css({
+    $('#set3').on('mouseover', function () {
+        $('#item6').css({
+            "background-color": "#ff1"
+        });
+        $('#item7').css({
             "background-color": "#ff1"
         });
     });
-    $('#item3').on('mouseout', function () {
-        $(this).css({
+    $('#set3').on('mouseout', function () {
+        $('#item6').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item7').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item4').on('mouseover', function () {
-        $(this).css({
+    $('#set4').on('mouseover', function () {
+        $('#item8').css({
+            "background-color": "#ff1"
+        });
+        $('#item9').css({
             "background-color": "#ff1"
         });
     });
-    $('#item4').on('mouseout', function () {
-        $(this).css({
+    $('#set4').on('mouseout', function () {
+        $('#item8').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item9').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item5').on('mouseover', function () {
-        $(this).css({
+    $('#set5').on('mouseover', function () {
+        $('#item10').css({
+            "background-color": "#ff1"
+        });
+        $('#item11').css({
             "background-color": "#ff1"
         });
     });
-    $('#item5').on('mouseout', function () {
-        $(this).css({
+    $('#set5').on('mouseout', function () {
+        $('#item10').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item11').css({
+            "background-color": "#fff"
+        });
     });
 
-    $('#item6').on('mouseover', function () {
-        $(this).css({
+
+    $('#set6').on('mouseover', function () {
+        $('#item12').css({
+            "background-color": "#ff1"
+        });
+        $('#item13').css({
             "background-color": "#ff1"
         });
     });
-    $('#item6').on('mouseout', function () {
-        $(this).css({
+    $('#set6').on('mouseout', function () {
+        $('#item12').css({
             "background-color": "#fff"
-        })
+        });
+        $('#item13').css({
+            "background-color": "#fff"
+        });
     });
 });
