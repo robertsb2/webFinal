8cce01869b52d9712c6559a333c602d5563fa5b0
Jessica <jbjabzin@gmail.com>
2018-03-09 11:04:26 -0700

    Worked on the design for the contacts page

---
+++ scripts/contact.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#link1').on('mouseover', function(){
+        $('#link1').css({
+            "background-color": "#F8DE7E",
+        });
+    });
+    $('#link1').on('mouseleave', function(){
+        $('#link1').css({
+            "background-color": "#ffc",
+        });
+    });
+    $('#link2').on('mouseover', function(){
+        $('#link2').css({
+            "background-color": "#F8DE7E",
+        });
+    });
+    $('#link2').on('mouseleave', function(){
+        $('#link2').css({
+            "background-color": "#ffc",
+        });
+    });
+    $('#link4').on('mouseover', function(){
+        $('#link4').css({
+            "background-color": "#F8DE7E",
+        });
+    });
+    $('#link4').on('mouseleave', function(){
+        $('#link4').css({
+            "background-color": "#ffc",
+        });
+    });
+    $('#link5').on('mouseover', function(){
+        $('#link5').css({
+            "background-color": "#F8DE7E",
+        });
+    });
+    $('#link5').on('mouseleave', function(){
+        $('#link5').css({
+            "background-color": "#ffc",
+        });
+    });
+});
