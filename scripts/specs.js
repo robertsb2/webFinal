f585a4f24c0b7306751c80a4cf407938c84696eb
Jessica <jbjabzin@gmail.com>
2018-03-11 11:55:37 -0600

    Mostly finished the design for the pages
    The only changes I still need to make have to be mostly centering things and tiny tweeks on some pages.

--- scripts/specs.js
+++ scripts/specs.js
@@ -1,121 +1,135 @@
 $(document).ready(function () {
+    $(".box").on('mouseover',function(){
+        $(this).css({
+            "background-color": "#F8DE7E",
+        });
+    });
+
+    $('.box').on('mouseleave', function(){
+        $(this).css({
+            "background-color": "#ffc",
+        });
+    });
+
+
+    //Table stuff
     $('#set0').on('mouseover', function () {
         $('#item0').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item1').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set0').on('mouseout', function () {
         $('#item0').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item1').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
     $('#set1').on('mouseover', function () {
         $('#item2').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item3').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set1').on('mouseout', function () {
         $('#item2').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item3').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
     $('#set2').on('mouseover', function () {
         $('#item4').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item5').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set2').on('mouseout', function () {
         $('#item4').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item5').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
     $('#set3').on('mouseover', function () {
         $('#item6').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item7').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set3').on('mouseout', function () {
         $('#item6').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item7').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
     $('#set4').on('mouseover', function () {
         $('#item8').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item9').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set4').on('mouseout', function () {
         $('#item8').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item9').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
     $('#set5').on('mouseover', function () {
         $('#item10').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item11').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set5').on('mouseout', function () {
         $('#item10').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item11').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 
 
     $('#set6').on('mouseover', function () {
         $('#item12').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
         $('#item13').css({
-            "background-color": "#ff1"
+            "background-color": "#F8DE7E"
         });
     });
     $('#set6').on('mouseout', function () {
         $('#item12').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
         $('#item13').css({
-            "background-color": "#fff"
+            "background-color": "#ffc"
         });
     });
 });
